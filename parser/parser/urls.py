cacefc675d5305df1dff62f48feca72fbbc3d8e7
Aksey010 <143290412+Aksey010@users.noreply.github.com>
2024-08-14 13:22:07 +0300

    Add files via upload

--- parser/parser/urls.py
+++ parser/parser/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hh_parser.urls', namespace='parser')),
     path('users/', include('usersapp.urls', namespace='users'))
-]
+] + debug_toolbar_urls()
